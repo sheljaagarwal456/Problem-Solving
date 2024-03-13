705b93c9021aa55bc34e10d29eaf9ed9b2557c4f
sheljaagarwal456 <124603561+sheljaagarwal456@users.noreply.github.com>
2024-03-13 17:00:21 +0000

    commit

--- Homework Swayam/Searching-1/counting_rotation.cpp
+++ Homework Swayam/Searching-1/counting_rotation.cpp
@@ -1,19 +1,15 @@
+//User function template for C++
 class Solution{
 public:	
 	int findKRotation(int arr[], int n) {
 	    // code here
-	    int low=0;
-	    int high=n-1;
-	    int mid=(low+high)/2;
-	    while(low<=high){
-	        if(arr[low]<=arr[mid]){
-	            low=mid+1;
-	        }
-	        else{
-	            if()
+	    int count=0;
+	    for(int i=0;i<n-1;i++){
+	        if(arr[i+1]<arr[i]){
+	            count=i+1;
 	        }
 	    }
-	    
+	    return count;
 	}
 
 };
