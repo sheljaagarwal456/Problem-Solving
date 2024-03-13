ae1f43d2d5afb7833885ff5896c7c0db544700b6
sheljaagarwal456 <124603561+sheljaagarwal456@users.noreply.github.com>
2024-03-13 16:58:24 +0000

    commit

---
+++ counting_rotation.cpp
@@ -0,0 +1,19 @@
+class Solution{
+public:	
+	int findKRotation(int arr[], int n) {
+	    // code here
+	    int low=0;
+	    int high=n-1;
+	    int mid=(low+high)/2;
+	    while(low<=high){
+	        if(arr[low]<=arr[mid]){
+	            low=mid+1;
+	        }
+	        else{
+	            if()
+	        }
+	    }
+	    
+	}
+
+};
