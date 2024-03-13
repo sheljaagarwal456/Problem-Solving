f2c76d46d7102d16736ec044fd80ac9ac269fc53
sheljaagarwal456 <124603561+sheljaagarwal456@users.noreply.github.com>
2024-03-13 19:12:54 +0000

    commit

---
+++ Homework Swayam/Searching-2/single_element_in_a_sorted_array.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int n=nums.size();
+
+        //size=1
+        if(n==1){
+            return nums[0];
+        }
+
+        //ans is first element
+        if(nums[0]!=nums[1]){
+            return nums[0];
+        }
+
+        //ans is last element
+        if(nums[n-1]!=nums[n-2]){
+            return nums[n-1];
+        }
+
+        //binary search
+        int s=0;
+        int e=nums.size()-1;
+        int mid=(s+e)/2;
+        while(s<=e){
+            if(nums[mid-1]!=nums[mid] && nums[mid]!=nums[mid+1]){
+               return nums[mid];
+            }
+
+            //right part me kab jaana hoga
+            else if(mid%2!=0 && nums[mid-1]==nums[mid]  ||  mid%2==0 && nums[mid]==nums[mid+1]){
+                s=mid+1;
+            }
+            //left me kab jaana hoga
+            else{
+                e=mid-1;
+            }
+            mid=(s+e)/2;
+        }
+        return -1;
+    }
+};
