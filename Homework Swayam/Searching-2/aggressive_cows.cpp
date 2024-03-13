7e0db7e76712dbc65bd1898bf829d8703fa504e5
sheljaagarwal456 <124603561+sheljaagarwal456@users.noreply.github.com>
2024-03-13 17:18:38 +0000

    commit

---
+++ Homework Swayam/Searching-2/aggressive_cows.cpp
@@ -0,0 +1,55 @@
+
+class Solution {
+public:
+
+    bool ispossible(vector<int> &stalls, int k, int mid )
+{
+    int cowcount=1;
+    int lastpos=stalls[0];
+    for(int i=0;i<stalls.size();i++)
+    {
+        if (stalls[i]-lastpos>=mid)
+        {
+            cowcount++;
+            if(cowcount==k)
+            {
+                return true;
+            }
+            lastpos=stalls[i];
+
+        }
+       
+    }
+     return false;
+}
+
+    int solve(int n, int k, vector<int> &stalls) {
+    
+        // Write your code here
+        sort(stalls.begin(),stalls.end());
+        int s=0;
+        int maxi=-1;
+        for(int i=0;i<stalls.size(); i++)
+        {
+            maxi=max(maxi,stalls[i]);
+             
+        }
+        int e=maxi;
+        int ans=-1;
+        int mid=s+((e-s)/2);
+        while(s<=e){
+            if (ispossible(stalls,k,mid)){
+                ans=mid;
+                s=mid+1;
+    
+            }
+            else{
+                e=mid-1;
+    
+            }
+            mid=s+(e-s)/2;
+    
+        }
+        return ans; 
+        }
+};
