aa4e8db041c8c5795e6052ce7584f3707405b92a
sheljaagarwal456 <124603561+sheljaagarwal456@users.noreply.github.com>
2024-03-23 17:25:27 +0000

    commit

---
+++ Homework Swayam/Trees1&2/same_tree.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(p==NULL&&q==NULL){
+            return true;
+        }
+        if(p==NULL&&q!=NULL){
+            return false;
+        }
+        if(p!=NULL&&q==NULL){
+            return false;
+        }
+        bool left=isSameTree(p->left,q->left);
+        bool right=isSameTree(p->right,q->right);
+        
+        bool value=p->val==q->val;
+        
+        if(left&&right&&value){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+};
