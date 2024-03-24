dd736ee1693267a093b6a02b725e158bc4afb6fe
sheljaagarwal456 <124603561+sheljaagarwal456@users.noreply.github.com>
2024-03-24 23:15:16 +0000

    commit

---
+++ Homework Swayam/Trees1&2/bottom_view.cpp
@@ -0,0 +1,35 @@
+
+class Solution {
+  public:
+    vector <int> bottomView(Node *root) {
+        vector<int>ans;
+        if (root==NULL){
+            return ans;
+        }
+        map<int,int>topnode;
+        queue<pair<Node*,int>>q;
+        
+        q.push(make_pair(root,0));
+        
+        while(!q.empty()){
+            pair<Node*,int>temp=q.front();
+            q.pop();
+            Node*frontnode=temp.first;
+            int hd=temp.second;
+            
+            //if one value is present for hd, do nothing 
+            topnode[hd]=frontnode->data;
+            if(frontnode->left){
+                q.push(make_pair(frontnode->left,hd-1));
+            }
+            if(frontnode->right){
+                q.push(make_pair(frontnode->right,hd+1));
+            }
+        }
+        
+        for(auto i:topnode){
+            ans.push_back(i.second);
+        }
+        return ans;
+    }
+};
