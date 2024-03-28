8123dded8a838775128795a73e01b874bdb69238
sheljaagarwal456 <124603561+sheljaagarwal456@users.noreply.github.com>
2024-03-28 16:37:49 +0000

    commit

---
+++ Homework Swayam/Trees3/right_view.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    //Function to return list containing elements of right view of binary tree.
+        void solve(TreeNode*root,vector<int>&ans,int level){
+        if(root==NULL){
+            return ;
+        }
+        if (level==ans.size()){
+            ans.push_back(root->val);
+        }
+        solve(root->right,ans,level+1);
+        solve (root->left,ans,level+1);
+    }
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int>ans;
+        solve(root,ans,0);
+        return ans;
+    }
+};
