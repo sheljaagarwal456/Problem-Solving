d40ece0b7ab4daca0c5f8a42e60f876031bc75e7
sheljaagarwal456 <124603561+sheljaagarwal456@users.noreply.github.com>
2024-04-05 20:09:34 +0000

    commit

--- Homework Swayam/Trees3/lowest_common_ancestor.cpp
+++ Homework Swayam/Trees3/lowest_common_ancestor.cpp
@@ -1,3 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
