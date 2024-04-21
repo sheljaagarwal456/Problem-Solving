dd24fd5a4a658e2afdac41b4a00493e022697c8b
sheljaagarwal456 <124603561+sheljaagarwal456@users.noreply.github.com>
2024-04-21 20:29:40 +0000

    commit

---
+++ Homework Swayam/graphs-3/detect_cycle_in_undirected_graph.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution{
+  public:
+    // Function to detect cycle in a directed graph.
+    bool isCyclic(int V, vector<int> adj[]) {
+        // code here
+        int indegree[V]={0};
+	    for(int i=0;i<V;i++){
+	        for(auto it:adj[i]){
+	            indegree[it]++;
+	            
+	        }
+	    }
+	    queue<int>q;
+	    for(int i=0;i<V ; i++){
+	        if(indegree[i]==0){
+	            q.push(i);
+	        }
+	    }
+	    int count=0;
+	    
+	    while(!q.empty()){
+	        int node=q.front();
+	        q.pop();
+	        count++;
+
+	        for(auto it:adj[node]){
+	            indegree[it]--;
+	            if(indegree[it]==0){
+	                q.push(it);
+	            }
+	        }
+	        
+	    }
+	    if(count==V){
+	        return false;
+	    }
+	    return true;
+    }
+};
